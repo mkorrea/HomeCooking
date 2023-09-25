8208d986e940a5629577ddeb9074e4b72d241408
mkorrea <mathscorrea@gmail.com>
2023-09-25 15:24:45 -0300

    imagens de fundo da tela inicial

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ var sol = document.getElementById('sol');
 var menu = document.getElementById('menu')
 var open = document.getElementById('openburguer')
 var close = document.getElementById('closeburguer')
+var bg = document.getElementById('html')
 var main = document.getElementById('main')
-
 
 function clickMenu() {
     if (menu.style.left == '0%') { 
@@ -14,44 +14,80 @@ function clickMenu() {
     } 
 }
 main.addEventListener("click", function fechar () {
-    if (menu.style.left == '0%') { 
+    if (menu.style.left == '-55%') { 
         menu.style.left = '-55%' 
     }
 })
 function mudou() {
-    if (window.innerWidth > 1200) {
+    if (window.innerWidth >= 1200) {
         menu.style.left = '0.1%'
     } else {
         menu.style.left = '-55%'
     }
 }
-
+var hom = document.getElementById('hom')
+hom.addEventListener('mouseenter', function entrou() {
+    bg.style.background = 'var(--background) url(images/bg-home.jpg) no-repeat center center fixed'
+    bg.style.backgroundSize = 'cover'
+})
+var sal = document.getElementById('sal')
+sal.addEventListener('mouseenter', function entrou() {
+    bg.style.background = 'var(--background) url(images/bg-sal.jpg) no-repeat center center fixed'
+    bg.style.backgroundSize = 'cover'
+})
+var doc = document.getElementById('doc')
+doc.addEventListener('mouseenter', function entrou() {
+    bg.style.background = 'var(--background) url(images/bg-doce.jpg) no-repeat center center fixed'
+    bg.style.backgroundSize = 'cover'
+})
 
 let modoEscuro = false;
 function modo() {
     modoEscuro = !modoEscuro;
-
+    
     if (modoEscuro) {
         lua.style.transform = 'rotate(180deg)';
         lua.style.opacity = '0'
         sol.style.transform = 'rotate(180deg)';
         sol.style.opacity = '1'
-        open.style.opacity = '1' 
+        open.style.color = 'white'
         close.style.color = 'white'
         menu.style.backgroundColor = 'var(--darkback)'
-        document.body.style.backgroundColor = 'var(--darkback)'
+        bg.style.background = 'var(--background) url(images/bg-dark-home-mb.jpg) no-repeat center center fixed'
+        document.body.style.background = 'rgba(0, 0, 0, 0.400)'
+        main.style.background = '#0f0f0fd8'
+        main.style.color = 'white'
     } else if (!modoEscuro) {
         lua.style.transform = 'rotate(0deg)';
         lua.style.opacity = '1'
         sol.style.transform = 'rotate(0deg)';
         sol.style.opacity = '0'
-        open.style.opacity = '0'
         lua.style.color = 'black'
+        open.style.color = 'black'
         close.style.color = 'black'
         menu.style.backgroundColor = 'rgb(230, 230, 230)'
-        document.body.style.backgroundColor = 'var(--background)'
+        bg.style.background = 'var(--background) url(images/bg-home-mb.jpg) no-repeat center center fixed'
+        document.body.style.background = 'rgba(0, 0, 0, 0.400)'
+        main.style.background = '#ffffffd8'
+        main.style.color = 'black'
     }
 }
+
+
+var tit = document.getElementById('titulo')
+var vol = document.getElementById('voltar')
+tit.addEventListener('mouseenter', function entrou() {
+    vol.style.top = '-2px'
+})
+tit.addEventListener('mouseleave', function saiu() {
+    vol.style.top = '-30px'
+})
+
+
+
+
+
+
 
 
 document.addEventListener("DOMContentLoaded", function () {
